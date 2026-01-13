6ad570b70d5bc0596f58abb438d81528d0fc813f
Edward Bender <ebender001@gmail.com>
2026-01-13 16:05:07 -0600

    Initial Commit

---
+++ EuroScore/EuroScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuroScoreApp.swift
+//  EuroScore
+//
+//  Created by Edward Bender on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct EuroScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
